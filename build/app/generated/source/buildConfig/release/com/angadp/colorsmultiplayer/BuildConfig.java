0fd24f71a1734460661b78c88333bd9537cbd45c
Angadpreet Nagpal <angadpreet.nagpal@gmail.com>
2020-05-24 20:34:57 +0530

    Android build

---
+++ build/app/generated/source/buildConfig/release/com/angadp/colorsmultiplayer/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.angadp.colorsmultiplayer;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.angadp.colorsmultiplayer";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
